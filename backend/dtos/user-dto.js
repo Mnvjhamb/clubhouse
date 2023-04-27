e3c8ad10a9be6003d2c1b9de3768999dfab2e523
manav <manavjhamb002@gmail.com>
2023-04-27 10:15:13 +0530

    ,,,,,,,,,,,,,,,,,,,,

--- backend/dtos/user-dto.js
+++ backend/dtos/user-dto.js
@@ -1,20 +1,21 @@
-class UserDto{
-    _id;
-    phone;
-    name;
-    profile;
-    activated;
-    createdAt;
+class UserDto {
+	_id;
+	phone;
+	email;
+	name;
+	profile;
+	activated;
+	createdAt;
 
-    constructor(user){
-        this.id = user._id;
-        this.phone = user.phone;
-        this.activated = user.activated;
-        this.createdAt = user.createdAt;
-        this.name = user.name;
-        this.profile = user.profile ?  `${user.profile}` : null;
-    }
-
+	constructor(user) {
+		this.id = user._id;
+		this.phone = user.phone;
+		this.email = user.email;
+		this.activated = user.activated;
+		this.createdAt = user.createdAt;
+		this.name = user.name;
+		this.profile = user.profile ? `${user.profile}` : null;
+	}
 }
 
 module.exports = UserDto;
